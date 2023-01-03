00c4391a70120504d67dc3f2e5cf7015e418407a
kohlsen <kohlsen@ascendcohealth.com>
2023-01-02 19:19:05 -0600

    inset-0

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Hero(props) {
   return (
-    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
+    <div className="flex items-center justify-center min-h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
       {/* Overlay */}
-      <div className="absolute top-0 right-0 left-0 bottom-0 bg-black/70 z-[2]" />
+      <div className="absolute top-0 right-0 left-0 bottom-0 inset-0 bg-black/70 z-[2]" />
       <div className="p-5 text-white z-[2] mt-[-10rem]">
         <h2 className="text-5xl font-bold text-red-300">{props.heading}</h2>
         <p className="py-5 text-xl text-gray-100">{props.message}</p>
